dc4a6276f46f4c59e1d01dd9fb05f21186c4c402
redrockjs <smb.sochi@gmail.com>
2023-01-12 16:25:20 +0300

    fix: add next-auth
    Took 3 hours 28 minutes

--- content/SignIn/SignIn.tsx
+++ content/SignIn/SignIn.tsx
@@ -1,9 +1,29 @@
 import s from "./styles.module.scss"
+import {useSession, signIn, signOut} from "next-auth/react"
 
 export function SignIn() {
+
+  const {data: session, status} = useSession()
+  const loading = status === "loading"
+
+
+  console.log(session)
+
   return <>
-    <main className={s.signin}>
-      SignIn
+    <main className={s.signIn}>
+      {session ? (
+        <>
+          Signed in as {session.user?.name} <br/>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <>
+          Not signed in <br/>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+
+
     </main>
   </>
 }
