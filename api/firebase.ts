f01ceffc38592fc3505f635c45b7be665a49c131
redrockjs <smb.sochi@gmail.com>
2023-01-13 15:42:34 +0300

    fix: save
    Took 59 minutes

--- api/firebase.ts
+++ api/firebase.ts
@@ -7,27 +7,27 @@ const ax = axios.create(
   });
 
 export let firebaseApi = {
-  getStorageItems(uid:string, authToken:string) {
+  getStorageItems(uid:string) {
     try {
-      return ax.get(`/fav/${uid}.json?auth=${authToken}`)
+      return ax.get(`/fav/${uid}.json?auth=${uid}`)
     } catch (e: unknown) {
       const err = e as Error | AxiosError
       throw new Error(err.message)
     }
   },
 
-  addStorageItem(uid:string, authToken:string, payload:string) {
+  addStorageItem(uid:string, payload:string) {
     try {
-      return ax.post(`/fav/${uid}.json?auth=${authToken}`, payload)
+      return ax.post(`/fav/${uid}.json?auth=${uid}`, payload)
     } catch (e: unknown) {
       const err = e as Error | AxiosError
       throw new Error(err.message)
     }
   },
 
-  delStorageItem (uid:string, authToken:string, key:string) {
+  delStorageItem (uid:string, key:string) {
     try {
-      return ax.delete(`/fav/${uid}/${key}.json?auth=${authToken}`)
+      return ax.delete(`/fav/${uid}/${key}.json?auth=${uid}`)
     } catch (e: unknown) {
       const err = e as Error | AxiosError
       throw new Error(err.message)
