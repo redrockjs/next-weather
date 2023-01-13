b3e7d4e3cf8e23f0814438d41a524e380c06d2c4
redrockjs <smb.sochi@gmail.com>
2023-01-13 14:11:04 +0300

    fix: add redux-toolkit
    Took 49 minutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {SessionProvider} from "next-auth/react"
+import {Provider} from "react-redux";
+import {store} from "../store/store";
 
 export default function App({
                               Component,
                               pageProps: {session, ...pageProps}
                             }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   )
 }
