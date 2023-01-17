b54a29f5d864ae10ed7b5c52dbed7cc03267f663
redrockjs <smb.sochi@gmail.com>
2023-01-17 15:25:00 +0300

    fix: add all asyncThunks
    Took 26 minutes

--- store/rootSlice.ts
+++ store/rootSlice.ts
@@ -15,40 +15,54 @@ export const fetchFavoriteCities = createAsyncThunk<FavoriteCitiesType[], string
     return await response.json()
   })
 
-export const addFavoriteCity = createAsyncThunk<FavoriteCitiesType[], string, { rejectValue: string }>(
-  'favoriteCities/fetchFavoriteCities',
-  async (uid, {rejectWithValue}) => {
-    const response = await fetch(`${DB_URL}/fav/${uid}.json?auth=${uid}`)
+export const addFavoriteCity = createAsyncThunk<FavoriteCitiesType, FavoriteCitiesType, { rejectValue: string, state: { uid: string } }>(
+  'favoriteCities/addFavoriteCity',
+  async (city, {rejectWithValue, getState}) => {
+
+    const uid = getState().uid
+
+    const response = await fetch(`${DB_URL}/fav/${uid}.json?auth=${uid}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application-json'
+      },
+      body: JSON.stringify(city)
+    })
 
     if (!response.ok) {
-      return rejectWithValue('Server Error!')
+      return rejectWithValue('Can\'t add city. Server Error!')
     }
 
     return await response.json()
   })
 
-export const removeFavoriteCity = createAsyncThunk<FavoriteCitiesType[], string, { rejectValue: string }>(
-  'favoriteCities/fetchFavoriteCities',
-  async (uid, {rejectWithValue}) => {
-    const response = await fetch(`${DB_URL}/fav/${uid}.json?auth=${uid}`)
+export const removeFavoriteCity = createAsyncThunk<number, number, { rejectValue: string, state: { uid: string } }>(
+  'favoriteCities/removeFavoriteCity',
+  async (id, {rejectWithValue, getState}) => {
+
+    const uid = getState().uid
+
+    const response = await fetch(`${DB_URL}/fav/${uid}/${id}.json?auth=${uid}`, {
+      method: 'DELETE'
+    })
 
     if (!response.ok) {
-      return rejectWithValue('Server Error!')
+      return rejectWithValue('Can\'t delete task. Server Error!')
     }
 
-    return await response.json()
+    return id
   })
-
-
 
 type InitialStateType = {
   isAuth: boolean
+  uid: string | null
   favoriteCities: FavoriteCitiesType[]
   loading: boolean
 }
 
 const initialState: InitialStateType = {
   isAuth: false,
+  uid: null,
   favoriteCities: [],
   loading: false
 }
@@ -62,6 +76,12 @@ const rootSlice = createSlice({
     },
     unsetIsAuth(state) {
       state.isAuth = false
+    },
+    setUid(state, action) {
+      state.uid = action.payload
+    },
+    unsetUid(state) {
+      state.uid = null
     },
     addFavorites(state, action: PayloadAction<FavoriteCitiesType>) {
       state.favoriteCities.push(action.payload)
@@ -79,6 +99,20 @@ const rootSlice = createSlice({
       })
       .addCase(fetchFavoriteCities.fulfilled, (state, action) => {
         state.favoriteCities = action.payload
+        state.loading = false
+      })
+      .addCase(addFavoriteCity.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(addFavoriteCity.fulfilled, (state, action) => {
+        //state.favoriteCities.push(action.payload)
+        state.loading = false
+      })
+      .addCase(removeFavoriteCity.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(removeFavoriteCity.fulfilled, (state, action) => {
+        state.favoriteCities = state.favoriteCities.filter(city => city.id !== action.payload)
         state.loading = false
       })
   }
