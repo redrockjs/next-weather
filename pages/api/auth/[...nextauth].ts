c988bf2f7ce068fa598e69b13eb8028629276fa4
redrockjs <smb.sochi@gmail.com>
2023-01-18 16:25:55 +0300

    fix: add firebase
    Took 53 minutes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import YandexProvider from "next-auth/providers/yandex"
 import VkProvider from "next-auth/providers/vk"
+import { FirestoreAdapter} from "@next-auth/firebase-adapter"
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -18,6 +19,16 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.VK_CLIENT_SECRET as string
     })
   ],
+  //TODO Не соединяется с БД, ошибка GRPC
+  adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  }),
   theme: {
     colorScheme: "light",
   },
