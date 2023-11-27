35d198414a3c8f0dcd7ac7c4cbbcd0340f133e5c
RedRock <smb.sochi@gmail.com>
2023-11-27 09:37:08 +0300

    fix: main

---
+++ src/components/common/Menu/Menu.tsx
@@ -0,0 +1,7 @@
+import s from './Menu.module.scss';
+
+function Menu() {
+  return <>Menu</>;
+}
+
+export default Menu;
--- src/view/Main/Main.tsx
+++ src/view/Main/Main.tsx
@@ -1,5 +1,17 @@
+import s from './Main.module.scss';
+
 function Main() {
-  return <>Main</>;
+  return (
+    <div className={s.Container}>
+      <div className={s.Wrapper}>
+        <div className={s.Menu}>Menu</div>
+        <div className={s.Logo}>Logo</div>
+        <div className={s.Search}>Search</div>
+        <div className={s.Profile}>Profile</div>
+        <div className={s.Main}>Main</div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
